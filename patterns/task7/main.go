3934d9f6c4cd461404ba3e1067ea615eaa7877bb
proggcreator <davidshibitov@gmail.com>
2021-08-12 11:28:57 +0300

    added strategy pattern

---
+++ patterns/task7/main.go
@@ -0,0 +1,21 @@
+package main
+
+func main() {
+
+	//algoritms for clean cache (capasity = 2)
+	lru := &lru{}
+	cache := initCache(lru)
+
+	// set algoritm LRU (Least Recently Used)
+	cache.setEvictionAlgo(lru)
+	cache.add("a", "1")
+	cache.add("b", "2")
+	cache.add("c", "3")
+
+	// set algoritm FIFO
+	fifo := &fifo{}
+	cache.setEvictionAlgo(fifo)
+
+	cache.add("d", "4")
+
+}
